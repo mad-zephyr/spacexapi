9391951a4105834f8c63271f9ff4c383dfa0ddc9
Vasilii Iatiuc <Vasilii.Iatiuc@endava.com>
2025-05-21 15:14:55 +0300

    initialize project

---
+++ src/test/java/com/nx/spacex/SpacexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nx.spacex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
